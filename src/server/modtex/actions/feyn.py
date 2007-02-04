24527fea34c2710152950292a1052a3422548af0
Peter Danenberg <pcd@rogygen.org>
2007-02-04 19:06:42 +0000

    actually add the feyn-bastards; no to mention: objective privileges

---
+++ src/server/modtex/actions/feyn.py
@@ -0,0 +1,51 @@
+##
+# mod_tex: distributed LaTeX-rendering over Apache
+# Copyright (C) 2007 Peter Danenberg
+# See doc/COPYING for details.
+#
+# @file
+# Contains the Feynman class for doing Feynman diagrams
+# over Thorsten Ohl's feynmf
+# (http://www.ctan.org/tex-archive/macros/latex/contrib/feynmf/).
+from modtex.action import Action
+from modtex.actions.latex import Latex
+from modtex.constants import Constants
+from modtex.types import Types
+from modtex.execution import Execution
+from modtex.config import *
+
+class Feyn(Latex):
+    NAME = Types.FEYN
+    TEMPLATE_FILE = Constants.TEMPLATE % \
+                    {'file': NAME,
+                     'suffix': Constants.MIMES[Types.LATEX].suffix}
+    FACILITY = Config.facilities[Types.LATEX]
+
+    def __init__(self, name=NAME, method=None, template_file=TEMPLATE_FILE,
+                 facility=FACILITY):
+        if not method:
+            method = self.render
+        super(Feyn, self).__init__(name, method, template_file, facility)
+
+    def render(self, reddendum):
+        IMAGE = '%(action)s.%(suffix)s'
+        METAPOST = '%(action)s-mp.%(suffix)s'
+        IMAGE_MIME = Constants.MIMES[Types.PNG]
+        SOURCE_MIME = Constants.MIMES[Types.LATEX]
+        METAPOST_MIME = Constants.MIMES[Types.METAPOST]
+
+        substituendum = {self.name: reddendum}
+        image = IMAGE % {'action': self.name,
+                         'suffix': IMAGE_MIME.suffix}
+        metapost_target = METAPOST % {'action': self.name,
+                               'suffix': METAPOST_MIME.suffix}
+        targets = {IMAGE_MIME.type: image,
+                   SOURCE_MIME.type: self.template_file}
+        latex = self.facility
+        metapost = Config.facilities[Types.METAPOST]
+        dvipng = Config.facilities[Types.DVIPNG]
+        executanda = [Execution(latex, args=[self.name]),
+                      Execution(metapost, args=[metapost_target]),
+                      Execution(latex, args=[self.name]),
+                      Execution(dvipng, args=['-o', image, self.name])]
+        return Action.render(self, substituendum, targets, executanda)
---
+++ src/server/modtex/privileges.py
@@ -0,0 +1,49 @@
+##
+# mod_tex: distributed LaTeX-rendering over Apache
+# Copyright (C) 2007 Peter Danenberg
+# See doc/COPYING for details.
+#
+# @file
+# Contains RootEscalationError and Privileges classes.
+from syslog import syslog
+from os import setregid, setgroups, setreuid, setuid, EX_NOPERM
+
+class RootEscalationError(Exception):
+    SAW = 'Process is yet able to become root'
+
+    def __init__(self, saw=SAW):
+        Exception.__init__(self, saw)
+
+class Privileges(object):
+    """ Composite of uid, gid """
+
+    uid = gid = code = 0
+
+    def __init__(self, uid, gid, code=0):
+        self.uid = uid
+        self.gid = gid
+        self.code = code
+
+    ##
+    # Taking our queue from sendmail; and Chen, Wagner, Dean
+    # (see <em>Setuid Demystified</em>, 2002): try to
+    # escalate back to root, and fail if this nought
+    # faileth.
+    def drop(self):
+        # Set group must be done before user.
+        setregid(self.gid, self.gid)
+        # Reduce supplementary groups
+        setgroups([self.gid])
+        # Finally, set user.
+        setreuid(self.uid, self.uid)
+        # Test that we cannot re-root
+        try:
+            setuid(0)
+        except OSError:
+            pass
+        else:
+            if code:
+                print >> stderr, RootEscalationError().message
+                exit(code)
+            else:
+                raise RootEscalationError()
